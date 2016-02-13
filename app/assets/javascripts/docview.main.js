0238d8c96ff09bad26dba2881bb42a6fd8de6e5f
Jeff Russ <jeffreylynnruss@gmail.com>
2016-02-13 04:32:05 +0000

    recalibrate scroll-to-hash top

--- app/assets/javascripts/docview.main.js
+++ app/assets/javascripts/docview.main.js
@@ -20,7 +20,7 @@
 
 // to fix navbar hiding of content when linking to spot in page:
 window.addEventListener("hashchange", function () {
-    scrollBy(0, -45);
+    scrollBy(0, -61);
 });
 
 $(document).ready(function () {
