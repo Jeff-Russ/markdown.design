e540ce4064bb10e4acecfd002fc283f575df20ba
Jeff Russ <jeffreylynnruss@gmail.com>
2016-05-10 04:33:28 -0400

    empty all contents to make master a dummy branch

--- config/initializers/string.rb
+++
@@ -1,5 +0,0 @@
-class String
-  def string_between_markers marker1, marker2
-    self[/#{Regexp.escape(marker1)}(.*?)#{Regexp.escape(marker2)}/m, 1]
-  end
-end
--- config/initializers/timeout.rb
+++
@@ -1,4 +0,0 @@
-Rack::Timeout.timeout = 20  # seconds
-
-# silence excessive logging for rack-timeout
-Rack::Timeout.unregister_state_change_observer(:logger) if Rails.env.development?
